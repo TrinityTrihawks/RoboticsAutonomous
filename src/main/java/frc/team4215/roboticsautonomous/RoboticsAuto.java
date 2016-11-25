ba28c24fd43919b54e91fdbf39e0e9cb7a693b41
Waweros <lion@waposta.com>
2016-11-25 16:16:34 -0600

    Created the new base module

---
+++ src/main/java/frc/team4215/roboticsautonomous/RoboticsAuto.java
@@ -0,0 +1,23 @@
+package frc.team4215.roboticsautonomous;
+
+import jaci.openrio.toast.lib.module.ToastModule;
+
+public class RoboticsAuto extends ToastModule {
+	
+	@Override
+	public String getModuleName() {
+		return "RobooticsAuto";
+	}
+
+	@Override
+    public String getModuleVersion() {
+        return "0.0.1";
+    }
+
+	@Override
+	public void prestart(){}
+
+	@Override
+	public void start(){}
+
+}
